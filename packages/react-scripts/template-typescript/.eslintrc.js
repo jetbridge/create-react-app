7a70323c54360c873f7622378180c2aa10718f73
Mischa Spiegelmock <mischa@mvstg.biz>
2019-07-29 11:59:17 +0300

    redundant react-app eslint

--- packages/react-scripts/template-typescript/.eslintrc.js
+++ packages/react-scripts/template-typescript/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'react-app'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
   plugins: ['@typescript-eslint', 'react'],
   parserOptions: {
     ecmaFeatures: {
