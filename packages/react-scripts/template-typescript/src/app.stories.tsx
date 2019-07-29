a9caabe86d11d08a86ba45d226ca6432ebf489f5
Mischa Spiegelmock <mischa@mvstg.biz>
2019-07-29 16:41:59 +0300

    don't really need MUI wrapper for app because it already wraps with theme

--- packages/react-scripts/template-typescript/src/app.stories.tsx
+++ packages/react-scripts/template-typescript/src/app.stories.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
-import { muiTheme } from 'storybook-addon-material-ui'
-import { theme } from './theme'
 import App from './app'
 
-storiesOf('App', module)
-  .addDecorator(muiTheme([theme]))
-  .add('Main', () => <App />)
+storiesOf('App', module).add('Main', () => <App />)
