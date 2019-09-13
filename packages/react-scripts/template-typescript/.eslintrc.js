bbf798affe535c2d5439dff39ad526b3afaab4eb
Mischa Spiegelmock <mischa@mvstg.biz>
2019-09-14 01:05:55 +0300

    typescript-eslint 2.0 is fucked

--- packages/react-scripts/template-typescript/.eslintrc.js
+++ packages/react-scripts/template-typescript/.eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
-  plugins: ['@typescript-eslint', 'react'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:react-app/recommended'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -12,16 +11,14 @@ module.exports = {
       version: 'detect',
     },
   },
+
   rules: {
     '@typescript-eslint/indent': ['warn', 2],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/explicit-member-accessibility': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/class-name-casing': 'warn', // withTheme, etc
     '@typescript-eslint/explicit-function-return-type': 'off', // just infer it...
-    'react/prop-types': 'off', // use TS
     '@typescript-eslint/member-delimiter-style': 'off',
     'jsx-a11y/alt-text': 'off',
     '@typescript-eslint/prefer-interface': 'off',
