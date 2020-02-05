8395521ff15e15a75b867bfcc92c159debb26842
Mischa Spiegelmock <mischa@mvstg.biz>
2020-02-05 17:18:52 +0200

    storybook

--- packages/cra-template-typescript/template/src/route/index.tsx
+++ packages/cra-template-typescript/template/src/route/index.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react'
-import { Route, Switch } from 'react-router-dom'
-import HomePage from '../page/Home'
+import * as React from "react"
+import { Route, Switch } from "react-router-dom"
+import HomePage from "../pages/Home"
 
 interface IRoutesProps {}
 
